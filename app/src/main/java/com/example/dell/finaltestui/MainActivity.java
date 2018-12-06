c914ed463c99077c32ba931e595d8ebe117281e8
Dell <kartik@utter.in>
2018-12-06 12:39:28 +0530

    con

--- app/src/main/java/com/example/dell/finaltestui/MainActivity.java
+++ app/src/main/java/com/example/dell/finaltestui/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.dell.finaltestui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 //new trial for data
+    //button created
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
